9e3b5b4c7298008b63b56c81f999556842129ed4
Ry-leigh <ryanaarondc@gmail.com>
2026-03-17 18:03:51 +0800

    Init: setup React Native project with Expo
    • Installed Zustand, Axios and NativeWind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        accent: '#AB8BFF'
+      }
+    },
+  },
+  plugins: [],
+}
